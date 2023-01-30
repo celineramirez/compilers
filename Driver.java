305720ff17ebe5b909697452a076c980742bfc55
celineramirez <80066301+celineramirez@users.noreply.github.com>
2023-01-30 16:58:11 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,26 +1,29 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.io.*;
 
 public class Driver {
     public static void main (String args[]) {
-    	
-    	// initialize token input stream
-    	ANTLRInputStream input = new ANTLRInputStream(System.in);
 
-        // initialize lexer
-        Little lexer = new Little(input);
-        
-        // intialize token buffer
-        CommonTokenStream token = new CommonTokenStream(lexer);
-        
-        // print tokens (incomplete)
-        while(token.hasMoreTokens()) {
-        	System.out.println(token.nextToken());
+        try {
+            // initialize token input stream
+            ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+            // initialize lexer
+            Little lexer = new Little(input);
+
+            while(true) {
+                Token token = lexer.nextToken();
+                if(token.getType() == Little.EOF){
+                    break;
+                }
+
+                System.out.println("TOKEN:" + token.getType() + "\n Value:" + token.getText());
+            }
         }
-        
-//      System.out.println("TOKEN:" + getAllTokens().getText() + "\n Value:" getAllTokens(i).value());
-//      System.out.println(getAllTokens().get(i)+ "\n");
-
+        catch(IOException e) {
+            System.out.println("IO Exception");
+        }
 
     }
 }
