525ac8025393f50d5bba6df3a93280e6d496aaf6
celineramirez <80066301+celineramirez@users.noreply.github.com>
2023-02-22 16:37:38 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,72 +5,40 @@ import java.io.*;
 public class Driver {
     public static void main (String args[]) {
 
-        int type = 0;
-
         try {
             // initialize token input stream
             ANTLRInputStream input = new ANTLRInputStream(System.in);
 
-            // initialize lexer
-            Little lexer = new Little(input);
-
-            while(true) {
-                Token token = lexer.nextToken();
-                if(token.getType() == Little.EOF){
-                    break;
-                }
-
-                type = token.getType();
-
-                System.out.print("Token Type: ");
-                switch(type) {
-                    case 1:
-                        System.out.println("KEYWORDS");
-                        break;
-                    case 2:
-                        System.out.println("IDENTIFIER");
-                        break;
-                    case 3:
-                        System.out.println("INTLITERAL");
-                        break;
-                    case 4:
-                        System.out.println("FLOATLITERAL");
-                        break;
-                    case 5:
-                        System.out.println("STRINGLITERAL");
-                        break;
-                    case 6:
-                        System.out.println("COMMENT");
-                        break;
-                    case 7:
-                        System.out.println("OPERATORS");
-                        break;
-                    case 8:
-                        System.out.println("WS");
-                        break;
-                    default:
-                        break;
-                }
-
-                type = 0;
-                System.out.println("Value: " + token.getText());
-
-                Little parser = new Little(token);
-                ParseTree tree;
-                System.out.println(tree.toStringTree(parser));
-
-                while(tree.getChild(token) != null) {
-                    switch(type){
-
-                        case 1:
-                            
-
-                }
+            System.out.println(matchInput(input));
+            if (matchInput(input)) {
+                System.out.println("Accepted");
+            } else {
+                System.out.println("Not accepted");
             }
+
         }
         catch(IOException e) {
-            System.out.println("IO Exception");
+            e.printStackTrace();
         }
 
-    }
-}
+    }// end main
+
+    public static boolean matchInput(CharStream input){
+
+        try{
+            // initialize lexer and parser
+            LittleLexer lexer = new LittleLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            LittleParser parser = new LittleParser(tokens);
+
+            parser.program();
+            return true;
+        }
+
+        catch(RecognitionException e){
+            return false;
+        }
+
+    }// end matchInput
+
+}// end Driver
