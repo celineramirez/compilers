025315c160c70174b7349bd55b53033f8f68252f
celineramirez <crmrz28@gmail.com>
2023-04-17 13:23:34 -0400

    updated simpleTableBuilder. Need to fix test3

--- src/SimpleTableBuilder.java
+++ src/SimpleTableBuilder.java
@@ -2,61 +2,14 @@ import java.util.*;
 
 public class SimpleTableBuilder extends LittleBaseListener {
 
-    ArrayList<String> codeList = new ArrayList<>();
-    ArrayList<HashMap> tableList = new ArrayList();
-    AbstractSyntaxTree ast = new AbstractSyntaxTree();
+    ArrayList<String> codeList = new ArrayList<>(); // list of IR Code
 
     // global constants
-    String[] varlist = null; // gather variable list to check which
-    String type = null; // variable type, need for READ and WRITE stmts
-    int reg_count = 0;
-    String arith_val = null;
-
-//    @Override
-//    public void enterPrimary(LittleParser.PrimaryContext ctx) {
-//        String val = ctx.getText();
-//        String intval, floatval = null;
-//        String op = null;
-//
-//        if (ctx.INTLITERAL() != null) {
-//            intval = ctx.getText();
-////            ast.insert(intval);
-//        } else if (ctx.FLOATLITERAL() != null) {
-//            floatval = ctx.getText();
-////            ast.insert(floatval);
-//        } else if (val.compareTo("(") == 0) {
-//
-//            if (!ctx.expr().expr_prefix().addop().isEmpty()) {
-//                op = ctx.expr().expr_prefix().addop().getText();
-//            } else if (!ctx.expr().factor().factor_prefix().mulop().isEmpty()) {
-//                op = ctx.expr().factor().factor_prefix().mulop().getText();
-//            }
-//
-//            switch (op) {
-//                case "+":
-//                    ast.insert("ADDI ");
-//                case "-":
-//                    ast.insert("SUBI ");
-//                case "*":
-//                    ast.insert("MULTI ");
-//                case "/":
-//                    ast.insert("DIVI ");
-//
-//                default:
-//                    System.out.println("null value");
-//            }
-//
-//            if (ctx.INTLITERAL() != null) {
-//                intval = ctx.getText();
-//                ast.insert(intval);
-//            } else if (ctx.FLOATLITERAL() != null) {
-//                floatval = ctx.getText();
-//                ast.insert(floatval);
-//            }
-//        }
-//
-//    }
-
+    String[] varlist = null; // gather variable list
+    String type = null; // Get the type of the variables collected in varlist
+    int reg_count = 0; // register counter
+    String arith_val = null; // gets the current id/literal being parsed
+    String[] string_list = null;
 
     public void enterProgram(LittleParser.ProgramContext ctx) {
         codeList.add(";IR code");
@@ -72,7 +25,6 @@ public class SimpleTableBuilder extends LittleBaseListener {
         if (text.contains("BEGIN")) {
             codeList.add(";LINK");
         }
-
     }
 
     @Override
@@ -81,23 +33,25 @@ public class SimpleTableBuilder extends LittleBaseListener {
     }
 
     @Override
-    public void enterPrimary(LittleParser.PrimaryContext ctx) {
-        arith_val = ctx.getText();
-    }
-
-    @Override
     public void enterVar_decl(LittleParser.Var_declContext ctx) {
         type = ctx.var_type().getText();
         varlist = ctx.id_list().getText().split(",");
+        System.out.println(Arrays.toString(varlist));
+    }
+
+    @Override public void enterString_decl(LittleParser.String_declContext ctx) {
+        string_list = ctx.id().getText().split(",");
+
     }
 
     // enter a read statement
     @Override
     public void enterRead_stmt(LittleParser.Read_stmtContext ctx) {
 
+        // get the variables in the read statement
         String[] read_Var = ctx.id_list().getText().split(",");
-        //get the type of the parameters in the read statement
-        //if float, add READF
+
+        // if float, READF
         for (String var : read_Var) {
             if (type.compareTo("FLOAT") == 0) {
                 codeList.add(";READF " + var);
@@ -106,12 +60,14 @@ public class SimpleTableBuilder extends LittleBaseListener {
             else if (type.compareTo("INT") == 0) {
                 codeList.add(";READI " + var);
             }
+
+            //condition if string
         }
-        // copy this same algorithm to enterWrite_stmt
     }
 
     @Override
     public void enterWrite_stmt(LittleParser.Write_stmtContext ctx) {
+        // same as read_stmt algorithm
         String[] write_Var = ctx.id_list().getText().split(",");
         for (String var : write_Var) {
             if (type.compareTo("FLOAT") == 0) {
@@ -119,141 +75,112 @@ public class SimpleTableBuilder extends LittleBaseListener {
             } else if (type.compareTo("INT") == 0) {
                 codeList.add(";WRITEI " + var);
             }
+            else if(string_list.equals(var)){
+                codeList.add(";WRITES" + var);
+            }
         }
     }
 
     @Override
+    public void enterPrimary(LittleParser.PrimaryContext ctx) {
+        arith_val = ctx.getText(); // get current expression value
+    }
+
+    @Override
     public void enterAssign_expr(LittleParser.Assign_exprContext ctx) {
-        String op = null;
-        String op_code = null;
-        String register = null;
 
-        // get the operator
-        if (ctx.expr().expr_prefix().addop() != null && !ctx.expr().expr_prefix().addop().isEmpty()) {
-            op = ctx.expr().expr_prefix().addop().getText();
-        } else if (ctx.expr().factor().factor_prefix().mulop() != null && !ctx.expr().factor().factor_prefix().mulop().isEmpty()) {
-            op = ctx.expr().factor().factor_prefix().mulop().getText();
+        String op = null; // operator currently being parsed
+        String op_code; // IR Code operator
+        String register; // append to reg_count, register to store current value in
+        String leftHandSide; // get the left hand side of the assignment statement
+        String expr; // get entire right hand side
+        String[] the_expr; // tokenize rhs
+
+        // make new register for assignments
+        reg_count++;
+        register = "$T" + reg_count;
+        leftHandSide = ctx.id().getText();
+
+        expr = ctx.expr().getText(); // get entire rhs
+        the_expr = expr.split("[+]|[-]|[*]|[/]"); // get operands
+        System.out.println(expr);
+
+        // check if parenthesis exist
+        if (expr.contains("(")) {
+            op = expr.substring(2, 3);
+            expr = expr.replaceAll("[()]", "");
+            the_expr = expr.split("[+]|[-]|[*]|[/]");
+        } else {
+            // get the operator
+            if (ctx.expr().expr_prefix().addop() != null && !ctx.expr().expr_prefix().addop().isEmpty()) {
+                op = ctx.expr().expr_prefix().addop().getText();
+            } else if (ctx.expr().factor().factor_prefix().mulop() != null && !ctx.expr().factor().factor_prefix().mulop().isEmpty()) {
+                op = ctx.expr().factor().factor_prefix().mulop().getText();
+            }
         }
 
-        String prim = null;
-
-//        LittleParser.PrimaryContext primaryContext = ctx.expr().factor().postfix_expr().primary();
-//        if (primaryContext != null) {
-////            if (!primaryContext.isEmpty()) {
-////                prim = primaryContext.getText();
-////            }
-//            if (primaryContext.id() != null && !primaryContext.id().isEmpty()) {
-//                prim = primaryContext.id().getText();
-//            }
-//            else if (primaryContext.INTLITERAL() != null) {
-//                prim = primaryContext.INTLITERAL().getText();
-//            }
-//            else if (primaryContext.FLOATLITERAL() != null) {
-//                prim = primaryContext.FLOATLITERAL().getText();
-//            }
-//        }
-//        System.out.println("prim " + prim);
-
-//        // check if parenthesis exist
-//            if(ctx.expr().expr_prefix().factor().postfix_expr().primary() != null &&
-//                    !ctx.expr().expr_prefix().factor().postfix_expr().primary().isEmpty()){
-//                prim = ctx.expr().expr_prefix().factor().postfix_expr().primary().getText();
-//
-//            }
-//        // check if variable
-//        else if (ctx.expr().expr_prefix().factor().postfix_expr().primary().id() != null &&
-//                !ctx.expr().expr_prefix().factor().postfix_expr().primary().id().isEmpty()){
-//            prim = ctx.expr().expr_prefix().factor().postfix_expr().primary().id().getText();
-//        }
-//        else if (ctx.expr().expr_prefix().factor().postfix_expr().primary().INTLITERAL() != null){
-//            prim = ctx.expr().expr_prefix().factor().postfix_expr().primary().INTLITERAL().getText();
-//        }
-//        else if (ctx.expr().expr_prefix().factor().postfix_expr().primary().FLOATLITERAL() != null){
-//            prim = ctx.expr().expr_prefix().factor().postfix_expr().primary().FLOATLITERAL().getText();
-//        }
-//        System.out.println(prim);
-
-
-        String leftHandSide = null;
-
-        if (ctx.getText().contains(":=")) {
-            reg_count++;
-            register = "T" + reg_count;
-            leftHandSide = ctx.id().getText();
-        }
-
-        // unary assignment expression
+        // unary assignment, a := 1
         if (op == null) {
             storeVars(ctx, register, op, leftHandSide);
         }
 
-
-        // binary assignment expressions
+        // operator exists, binary assignment a := b + c
         else {
 
-            String expr = null;
-            String[] the_expr = null;
-            String arithStmt = null;
-            String t = VarType();
+            String arithStmt; // IR code to enter in codeList
+            String t = VarType(); // type of variable currently being parsed
 
             switch (op) {
 
                 case "+":
                     op_code = ";ADD" + t;
-                    expr = ctx.expr().getText();
-                    the_expr = expr.split("[+]");
 
                     if (the_expr.length == 2) {
-                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " $" + register;
+                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " " + register;
                         codeList.add(arithStmt);
                     }
                     break;
                 case "-":
                     op_code = ";SUB" + t;
-                    expr = ctx.expr().getText();
-                    the_expr = expr.split("[-]");
 
                     if (the_expr.length == 2) {
-                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " $" + register;
+                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " " + register;
                         codeList.add(arithStmt);
                     }
                     break;
                 case "*":
                     op_code = ";MULT" + t;
-                    expr = ctx.expr().getText();
-                    the_expr = expr.split("[*]");
 
                     if (the_expr.length == 2) {
-                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " $" + register;
+                        arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " " + register;
                         codeList.add(arithStmt);
                     }
                     break;
 
                 case "/":
 
+                    String reg;
+                    int regc = reg_count;
                     String store = null;
                     op_code = ";DIV" + t;
+                    String store_code = ";STORE" + t;
 
+                    // if div does not contain literal, don't change register count
+                    // otherwise use original register count and place into register 1 up
                     System.out.println("LINE 237: Need to figure out division algorithm for test3\n");
-                    if (isLit().compareTo("int") == 0) {
-                        store = ";STOREI " + "$" + register + " " + arith_val +
-                                "\n" + op_code + "$" + register + " $T";
-                        int reg = reg_count+1;
-                        store += reg;
+
+                    if (isLit()) {
+                        store = store_code + register + " " + arith_val +
+                                "\n" + op_code + " " + register + " $T";
+                        reg_count++;
+                        store += reg_count;
                         codeList.add(store);
-                    } else if (isLit().compareTo("float") == 0) {
-                        store = ";STOREF " + "$" + register + " " + arith_val +
-                                "\n"+ op_code + "$" + register + " $T";
-                        int reg = reg_count+1;
-                        store += reg;
-                        codeList.add(store);
-                    }
-                    else {
+                    } else {
                         expr = ctx.expr().getText();
                         the_expr = expr.split("[/]");
 
                         if (the_expr.length == 2) {
-                            arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " $" + register;
+                            arithStmt = op_code + the_expr[0] + " " + the_expr[1] + " " + register;
                             codeList.add(arithStmt);
                         }
                     }
@@ -262,73 +189,50 @@ public class SimpleTableBuilder extends LittleBaseListener {
             // There is a store at the end of every arithmetic operation
             storeVars(ctx, register, op, leftHandSide);
         }
-    }
+    }// end assign_expr
 
     public String VarType() {
+        // return the variable type initial, appends to op_code in assign_expr
         if (type.compareTo("FLOAT") == 0) {
             return "F ";
         } else if (type.compareTo("INT") == 0) {
             return "I ";
         }
-
         return " ";
     }
 
+    // Write STORE ir code
     public void storeVars(LittleParser.Assign_exprContext ctx, String register, String op, String lhs) {
         String t = VarType();
-        String op_code = null;
-        String store = null;
+        String op_code;
+        String store;
         op_code = ";STORE" + t;
 
         if (op == null) {
-            store = op_code + ctx.expr().getText() + " $" + register + "\n" +
-                    op_code + "$" + register + " " + lhs;
+            store = op_code + ctx.expr().getText() + " " + register + "\n" +
+                    op_code + register + " " + lhs;
             codeList.add(store);
         } else { // for end of arithmetic operations
-            store = op_code + "$" + register + " " + lhs;
+            store = op_code + register + " " + lhs;
             codeList.add(store);
         }
-
-
     }
 
-    public String isLit() {
-
+    // check if integer or float literal, for DIV only
+    public Boolean isLit() {
+        float val = 0;
         try {
-            float val = Float.parseFloat(arith_val);
-            if(val % 1 != 0){
-                return "float";
-            }
-            else{
-                return "int";
-            }
+            val = Float.parseFloat(arith_val);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        catch(Exception e) {
-            return " ";
-        }
-    }
-
-    @Override
-    public void enterString_decl(LittleParser.String_declContext ctx) {
-
     }
 
     public void prettyPrint() {
-        //print all symbol tables in the order they were created
-        //print out symlist in order tables were made
-
-//            for (int i=0; i<tableList.size(); i++) {
-//                HashMap<String,ArrayList> curr = tableList.get(i);
-//
-//                curr.entrySet().forEach(entry->{
-//                    System.out.println(entry.getKey() + " " + entry.getValue());
-//                });
-//            }// end for
-
-        for (int i = 0; i < codeList.size(); i++) {
-            String code = codeList.get(i);
+        // print the IR code
+        for (String code : codeList) {
             System.out.println(code);
         }
-
     }// end print
-}
+}// end class
